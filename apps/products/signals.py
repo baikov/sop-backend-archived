1448cc58e9b8bbe5e785c6a79526ca36d1e53b11
Alexey Baikov <baikov52@gmail.com>
2023-05-02 12:22:09 +0300

    SOP-3. Added signals for prices calculation

--- apps/products/signals.py
+++ apps/products/signals.py
@@ -1,68 +1,69 @@
+import math
+
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from slugify import slugify
 
 from apps.products.models import Product, ProductPropertyValue
-from apps.products.services.products import (
-    add_product_properties,
-    remove_redundant_product_properties,
-)
+
+# from apps.products.services.products import (
+#     add_product_properties,
+#     remove_redundant_product_properties,
+# )
 
 
 @receiver(pre_save, sender=Product)
-def calculate_prices_signal(sender, instance, **kwargs):
-    # 3. Калькуляция цен
-    # 4. Заполнение slug + уникализация
-
+def generate_slug_signal(sender, instance, **kwargs):
     if instance.slug == "":
         instance.slug = slugify(instance.name)
-    # replacements=[['|', 'or'], ['%', 'percent']]
-    # if instance.pk:
-    #     try:
-    #         orig = sender.objects.get(id=instance.id)
-    #         if orig:
-    #             changes = 0
-    #             for field in input_fields:
-    #                 if not (getattr(instance, field)) == (getattr(orig, field)):
-    #                     changes += 1
-    #             if changes > 0:
-    #                 # do something here because at least one field changed...
-    #                 my_geocoder_function(instance)
-    #     except:
-    #         # do something here because there is no original, or pass.
-    #         my_geocoder_function(instance)
-
-    # else:
-    if instance.ton_price:
-        try:
-            meter_weight = instance.properties_through.get(
-                property__code="ves-metra"
-            ).value
-            instance.meter_price = (
-                instance.ton_price
-                / 1_000_000
-                * int(float(meter_weight.replace(",", ".")) * 1000)
-            )
-        except ProductPropertyValue.DoesNotExist:
-            pass
-        try:
-            unit_weight = instance.properties_through.get(
-                property__code="ves-shtuki"
-            ).value
-            instance.unit_price = (
-                instance.ton_price
-                / 1_000_000
-                * int(float(unit_weight.replace(",", ".")) * 1000)
-            )
-        except ProductPropertyValue.DoesNotExist:
-            pass
-        # instance.meter_price = instance.ton_price / 1_000_000 * int(meter_weight)
-        # instance.unit_price = instance.ton_price / 1_000_000 * int(unit_weight)
 
 
 @receiver(post_save, sender=Product)
 def manage_product_properties_signal(sender, instance, **kwargs):
-    # 1. Если выбрана категория - добавить нужные свойства к товару
-    add_product_properties(instance)
-    remove_redundant_product_properties(instance)
-    # 2. Если указана длина и вес метра - рассчитать вес штуки
+    """
+    Если указана главная категория - добавить нужные свойства к товару, удалить ненужные
+    """
+    # add_product_properties(instance)
+    # remove_redundant_product_properties(instance)
+    pass
+
+
+@receiver(post_save, sender=ProductPropertyValue)
+def calculate_prices_signal(sender, instance, **kwargs):
+    """
+    Если указана длина и вес тонны - рассчитываем вес штуки, цену метра и цену штуки
+    """
+    if instance.property.code == "ves-metra" and instance.product.ton_price:
+        instance.product.meter_price = math.ceil(
+            float(instance.product.ton_price)
+            / 1_000
+            * float(instance.value.replace(",", "."))
+        )
+
+        length = (
+            ProductPropertyValue.objects.filter(
+                product=instance.product,
+                property__code="dlina",
+            )
+            .first()
+            .value
+        )
+        if "-" in length:
+            length = length.split("-")[0]
+
+        if length:
+            instance.product.unit_price = math.ceil(
+                float(instance.product.meter_price) * float(length) / 1000
+            )
+        instance.product.save()
+
+    if instance.property.code == "dlina" and instance.product.meter_price:
+        length = instance.value
+        if "-" in length:
+            length = length.split("-")[0]
+
+        if length:
+            instance.product.unit_price = math.ceil(
+                float(instance.product.meter_price) * float(length) / 1000
+            )
+            instance.product.save()
