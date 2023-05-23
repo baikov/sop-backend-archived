ff5031af3bb38467e248f805976a18814e8185e9
Alexey Baikov <baikov52@gmail.com>
2023-05-23 22:57:17 +0300

    Hotfix. Сортировка списка продуктов по свойству

--- apps/products/services/categories.py
+++ apps/products/services/categories.py
@@ -1,8 +1,10 @@
+from django.db.models import CharField, F, FloatField, Func, OuterRef, Subquery, Value
+from django.db.models.functions import Cast
 from django.db.models.query import QuerySet
 from rest_framework.exceptions import NotFound
 
 from apps.products.filters import ProductFilter
-from apps.products.models import Category
+from apps.products.models import Category, ProductPropertyValue
 from apps.utils.custom import get_object_or_None
 
 
@@ -37,4 +39,38 @@ def get_category_product_list(slug: str, filters: dict = None) -> QuerySet:
     if category is None:
         raise NotFound(f"Категория slug={slug} не существует")
     qs = category.products.filter(is_published=True)
+
+    first_property = category.product_properties.exclude(
+        code__in=[
+            "gost",
+            "marka-stali",
+            "poverkhnost",
+            "occvet",
+            "ves-metra",
+            "ves-shtuki",
+            "tolshina-stenki",
+        ]
+    ).first()
+    if first_property:
+        qs = qs.annotate(
+            property_value=Cast(
+                Subquery(
+                    ProductPropertyValue.objects.filter(
+                        property_id=first_property.id, product_id=OuterRef("pk")
+                    ).values(
+                        property_value=Func(
+                            F("value"),
+                            Value(","),
+                            Value("."),
+                            function="REPLACE",
+                            output_field=CharField(),
+                        )
+                    )[
+                        :1
+                    ]
+                ),
+                output_field=FloatField(),
+            )
+        ).order_by("property_value")
+
     return ProductFilter(filters, qs).qs
